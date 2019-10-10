21a841f6d4926010dec6caacbbb80e8e39dedde8
Jajodia <tjajodia@internal.tavisca.com>
2019-10-10 11:28:32 +0530

    connected to Local MySQL database

---
+++ src/main/java/com/tavisca/springapplication/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.tavisca.springapplication.repository;
+
+import com.tavisca.springapplication.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+}
